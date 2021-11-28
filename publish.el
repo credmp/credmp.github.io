867b31caf58b9486a7814f8c5a631a15e1e52c3f
Arjen Wiersma <arjen@wiersma.org>
2021-11-28 16:25:50 +0100

    :sparkles: Dealing with migration to elpa for org

--- publish.el
+++ publish.el
@@ -1,11 +1,11 @@
 (require 'package)
 (package-initialize)
 (unless package-archive-contents
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  ;;(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-refresh-contents))
-(dolist (pkg '(dash projectile org-plus-contrib yaml-mode htmlize php-mode clojure-mode haskell-mode rust-mode))
+(dolist (pkg '(dash projectile org org-contrib yaml-mode htmlize php-mode clojure-mode haskell-mode rust-mode))
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
