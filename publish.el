dca4ba33b58cf8e39acc60fe3c089e12b510f3f2
Arjen Wiersma <arjen@wiersma.org>
2021-11-28 15:54:10 +0100

    :snail: not yet available in CI

--- publish.el
+++ publish.el
@@ -1,7 +1,7 @@
 (require 'package)
 (package-initialize)
 (unless package-archive-contents
-  ;;(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-refresh-contents))
